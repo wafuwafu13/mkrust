cdc4429f7e01717f547849eb6e38a122fde4b109
wafuwafu13 <mariobaske@i.softbank.jp>
2022-10-04 19:15:03 +0900

    feat: receive hosts as arg

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,15 @@ use clap::Parser;
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+struct Cli {
+    command: Option<String>,
 }
 
 fn main() {
-    let args = Args::parse();
+    let cli = Cli::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    match cli.command.as_deref() {
+        Some("hosts") => println!("TODO: implement `mkrust hosts`"),
+        _ => unimplemented!(),
     }
 }
